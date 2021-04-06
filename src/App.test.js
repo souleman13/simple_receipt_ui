d8b918e183dd3c08b51f8acebf0300f4491b0bdf
Douglas Soule <souleman13@gmail.com>
2021-04-06 00:56:09 -0600

    adjust tests for future use

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Receipt UI/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//I want to provide more tests here but without the api endpoint working I have no unreliable piece of the application to test.
+//The above test simple ensure the header loads
